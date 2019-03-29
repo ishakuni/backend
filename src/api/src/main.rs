dd32e502cc738b5b449bceb80aba8030eea9b6a9
Pradip Caulagi <caulagi@gmail.com>
2019-03-29 11:28:15 +0100

    Api's will not talk to database
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,3 +1,28 @@
+extern crate actix;
+extern crate actix_web;
+extern crate env_logger;
+
+use actix_web::{middleware, server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    let sys = actix::System::new("hello-world");
+
+    server::new(|| {
+        App::new()
+            // enable logger
+            .middleware(middleware::Logger::default())
+            .resource("/", |r| r.f(index))
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .start();
+
+    println!("Started http server: 127.0.0.1:8080");
+    let _ = sys.run();
 }
--- src/api/src/schema.rs
+++
@@ -1,6 +0,0 @@
-table! {
-    customer (id) {
-        id -> Unsigned<Integer>,
-        username -> Text,
-    }
-}
